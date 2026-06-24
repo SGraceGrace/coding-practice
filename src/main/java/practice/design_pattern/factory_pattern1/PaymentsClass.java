6addcb549da0fffdbc83eaa4ff752e9d9708a4d5
SGraceGrace <143794400+SGraceGrace@users.noreply.github.com>
2026-06-24 22:30:05 +0530

    factory pattern 1

--- src/main/java/practice/design_pattern/factory_pattern1/PaymentsClass.java
+++ src/main/java/practice/design_pattern/factory_pattern1/PaymentsClass.java
@@ -1,13 +1,15 @@
 package practice.design_pattern.factory_pattern1;
 
-import practice.design_pattern.factory_pattern1.payment_factory.PaymentFactory;
-import practice.design_pattern.factory_pattern1.payment_factory.UPIPaymentFactory;
+import practice.design_pattern.factory_pattern1.payment_factory.PaymentProcessor;
+import practice.design_pattern.factory_pattern1.payment_factory.UPIPaymentProcessor;
+import practice.design_pattern.factory_pattern1.payments.PaymentInterface;
 
 public class PaymentsClass {
 
   public static void main(String[] args) {
-    PaymentFactory paymentFactory = new UPIPaymentFactory();
-    System.out.println(paymentFactory.payment());
+    PaymentProcessor paymentProcessor = new UPIPaymentProcessor();
+    PaymentInterface paymentInterface = paymentProcessor.createPayment();
+    System.out.println(paymentInterface.pay());
   }
 
 }
--- src/main/java/practice/design_pattern/factory_pattern1/payment_factory/PaymentProcessor.java
+++ src/main/java/practice/design_pattern/factory_pattern1/payment_factory/PaymentProcessor.java
@@ -4,11 +4,11 @@ import practice.design_pattern.factory_pattern1.payments.PaymentInterface;
 
 public abstract class PaymentProcessor {
 
-  protected abstract PaymentInterface createPayment();
+  public abstract PaymentInterface createPayment();
 
-  public String payment() {
-    PaymentInterface paymentInterface = createPayment();
-
-    return paymentInterface.pay();
-  }
+//  public String payment() {
+//    PaymentInterface paymentInterface = createPayment();
+//
+//    return paymentInterface.pay();
+//  }
 }
